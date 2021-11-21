d94c61470acb109de9653dd1c991ae9cf0e3ef5a
MUKUL BANSAL <curlvector747@gmail.com>
2021-11-21 12:00:57 +0530

    Create Java End-of-file.java

---
+++ Java End-of-file.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+         Scanner in=new Scanner(System.in);
+        String a;
+        int i=1;
+       while(in.hasNext())
+           {
+           a=in.nextLine();
+           System.out.println(i+" "+a);
+           i++;
+       }
+    }
+}
