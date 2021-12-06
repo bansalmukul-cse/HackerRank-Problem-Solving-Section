6f0df9e81317e7542f97c879a324c87424589804
MUKUL BANSAL <curlvector747@gmail.com>
2021-12-06 13:36:23 +0530

    Create List Comprehensions.py

---
+++ List Comprehensions.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    output=[]
+    for i in range(x+1):
+        for j in range(y+1):
+            for k in range(z+1):
+                if i+j+k==n:
+                    continue
+                else:
+                    output.append([i,j,k])
+    
+    print(output)
