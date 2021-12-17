ebe163fbc042895feb5c27cd31c6f8b302f6c986
MUKUL BANSAL <curlvector747@gmail.com>
2021-12-17 16:18:24 +0530

    Create Find a string.py

---
+++ Find a string.py
@@ -0,0 +1,12 @@
+def count_substring(string, sub_string):
+    count = 0
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count += 1
+    return count
+if __name__ == '__main__':
+    string = raw_input().strip()
+    sub_string = raw_input().strip()
+    
+    count = count_substring(string, sub_string)
+    print count
