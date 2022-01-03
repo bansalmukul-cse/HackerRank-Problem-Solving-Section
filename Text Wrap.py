58db8444d6f6057940ce928f3cd121adf1fe09d0
MUKUL BANSAL <curlvector747@gmail.com>
2022-01-03 12:20:31 +0530

    Create Text Wrap.py

---
+++ Text Wrap.py
@@ -0,0 +1,12 @@
+import textwrap
+
+def wrap(string, max_width):
+    s = ""
+    for c in range(0,len(string),max_width):
+        s += string[c:c+max_width] + "\n"
+    return s
+
+if __name__ == '__main__':
+    string, max_width = raw_input(), int(raw_input())
+    result = wrap(string, max_width)
+    print result
